78e00b8aca981af38e65a8dbd52c1e721b94ae01
mollytb <32122700+mollytb@users.noreply.github.com>
2018-05-18 04:14:02 -0400

    random movies
    random 3 movies route added, and title/actor/director route amended to include director

--- routes/movie-api-routes.js
+++ routes/movie-api-routes.js
@@ -1,33 +1,56 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
+const {gt, or, and, lte, ne, in: opIn} = Sequelize.Op;
 
-module.exports = function(app) {
-  app.get("/api/movies", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/movies", function (req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Review
     db.Movie.findAll({
-   //   include: [db.Review]
-    }).then(function(dbMovie) {
+      order: [
+      [Sequelize.fn('RAND')]
+    ],
+      limit: 3
+      //   include: [db.Review]
+    }).then(function (dbMovie) {
       res.json(dbMovie);
     });
   });
 
-  app.get("/api/movies/:id", function(req, res) {
+  app.get("/api/movies/:id", function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Review
-    db.Movie.findOne({
+    db.Movie.findAll({
       where: {
-        id: req.params.id
-      },
-//      include: [db.Review]
-    }).then(function(dbMovie) {
+        [or]: [{
+            actor_1_name: req.params.id
+          },
+          {
+            actor_2_name: req.params.id
+          },
+          {
+            actor_3_name: req.params.id
+          },
+          {
+            director_name: req.params.id
+          },
+          {
+            movie_title: req.params.id
+          }
+        ]
+
+      }
+
+      //      include: [db.Review]
+    }).then(function (dbMovie) {
       res.json(dbMovie);
     });
   });
 
-  app.post("/api/movies", function(req, res) {
-    db.Movie.create(req.body).then(function(dbMovie) {
+  app.post("/api/movies", function (req, res) {
+    db.Movie.create(req.body).then(function (dbMovie) {
       res.json(dbMovie);
     });
   });
