f9b9db762a8089f91211f0c2bf60bd5f6c2e4d9f
Molly Burns <Molly@Kathryns-MacBook-Pro.local>
2018-05-19 13:50:13 -0400

    table adjustments

--- public/js/movie.js
+++ public/js/movie.js
@@ -40,15 +40,14 @@ $(document).ready(function() {
     var newTr = $("<tr>");
     newTr.data("movie", movieData);
     newTr.append("<td>" + movieData.movie_title + "</td>");
-    newTr.append("<td>" + movieData.actor_2_name + movieData.actor_2_name + movieData.actor_3_name + "</td>");
+    newTr.append("<td>" + movieData.actor_1_name + movieData.actor_2_name + movieData.actor_3_name + "</td>");
     newTr.append("<td>" + movieData.director_name + "</td>");
     newTr.append("<td>" + movieData.movie_genres + "</td>");
     newTr.append("<td>" + movieData.imdb_score + "</td>");
-    newTr.append("<td>" + movieData.movie_title + "</td>");
+    newTr.append("<td><a href='/review?movie_id=" + movieData.id + "'>Reviews</a></td>");
+    
     if( movieData.Reviews !=null) 
       newTr.append("<td> " + movieData.Reviews.length + "</td>");
-    newTr.append("<td><a href='/blog?movie_id=" + movieData.id + "'>Go to Reviews</a></td>");
-    newTr.append("<td><a href='/cms?movie_id=" + movieData.id + "'>Create a Review</a></td>");
  
     return newTr;
   }
