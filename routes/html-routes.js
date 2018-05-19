65f96ed33d19f860191f20eb4f0d519f455ffe8c
mkondray <mkondray@gmail.com>
2018-05-19 12:02:59 -0400

    fixed a few html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,24 +12,24 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // index route loads home.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  // cms route loads cms.html
-  app.get("/cms", function(req, res) {
+  // random route loads random.html
+  app.get("/random", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/random.html"));
+  });
+
+  // review route loads review.html
+  app.get("/review", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/review.html"));
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  // movies route loads movie-manager.html
-  app.get("/movies", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/movie-manager.html"));
+  // search route loads search.html
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
   });
 
  // members route loads member-manager.html
